e66e549c01565e0ee51dd4ffabc0597d034f3a10
Jaloliddin <122007173+Jaloliddinnet@users.noreply.github.com>
2023-02-04 16:02:35 +0500

    Add

---
+++ Hakaton.Data/DBContext/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using HakatoApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HakatoApi.DBContext
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        { 
+        }
+
+        public DbSet<Doctor> doctor { get; set; }
+        public DbSet<Patient> patient { get; set; }
+        public DbSet<sickness> sickness { get; set; }
+    }
+}
